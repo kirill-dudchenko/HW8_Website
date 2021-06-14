da09d806ce9a13961e45ceec911c11cec3dbee49
Kirill Dudchenko <dudchenko.kirill.1995@gmail.com>
2021-06-14 19:05:36 +0300

    -

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ const server = express();
 const mainRoute = require('./routes/main.js');
 
 server.set('views', path.join(__dirname, 'views'));
+
 server.set('view engine', 'ejs');
 
 server.use(express.urlencoded({extended: false}));
+
 server.use(express.static(__dirname + '/public'));
+
 server.use('/main', mainRoute)
 
 server.listen(8000, () => {})
